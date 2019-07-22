65734790ea7963fd7e0d9857238ef47058b75046
daynine23 <kevinalmeyda17@gmail.com>
2019-07-21 19:01:11 -0500

    changes done!

--- __mocks__/test.js
+++
@@ -1,6 +0,0 @@
-const test = [
-    { id: 1, name: "Mamacona" },
-    { id: 2, name: "Punta Sal" },
-];
-
-module.exports = test;
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,23 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/111', function(request, response, next) {
 
-    const recipes = require('../__mocks__/recipes');
-    const foundRecipe = recipes.filter(recipe => recipe.name.includes(request.param('name')));
-    response.send(foundRecipe);
- 
- });
-
- router.get('/123', function(request, response, next) {
-
-    const tests = require('../__mocks__/test');
-    const foundTest = tests.filter(test => test.name.includes(request.param('name')));
-    response.send(tests);
- 
- });
-
- router.get('/321', function(request, response, next) {
+ router.get('/', function(request, response, next) {
 
     const recipes = require('../__mocks__/recipes');
     const foundRecipe = recipes.find(recipe => recipe.name.includes(request.param('name')));
