6c49ad5846652551c2885e4283b1b2a1e04d08ee
daynine23 <kevinalmeyda17@gmail.com>
2019-07-21 19:25:31 -0500

    last changes done

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,13 +1,4 @@
 const express = require('express');
 const router = express.Router();
 
-
- router.get('/', function(request, response, next) {
-
-    const recipes = require('../__mocks__/recipes');
-    const foundRecipe = recipes.find(recipe => recipe.name.includes(request.param('name')));
-    response.send(foundRecipe.id);
- 
- });
-
  module.exports = router;
